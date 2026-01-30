3763ac35d0bff1b01686bf327adfda77e78cd277
Gustavo Gualberto <gustavogualbertodasilva04@gmail.com>
2026-01-30 20:46:42 -0300

    corrigindo mais erros

--- Script1/modules/ui.lua
+++ Script1/modules/ui.lua
@@ -42,7 +42,7 @@ function UI.Init(Aimlock)
     botaoCorner.Parent = AimLockButton
 
     AimLockButton.MouseButton1Click:Connect(function()
-        Aimlock.Toggle(not aimlock.IsActive())
+        Aimlock.TurnOnOrOff(not aimlock.IsActive())
         if Aimlock.IsActive() then 
             AimLockButton.BackgroundColor3 = Color3.fromRGB(0, 70, 0)
         else
