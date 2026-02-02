24bd99bd1171eff0a91fadbdfd48e3de28924384
Gustavo Gualberto <gustavogualbertodasilva04@gmail.com>
2026-02-02 20:11:42 -0300

    Adicionando Botão Minimizar

--- Script1/modules/ui.lua
+++ Script1/modules/ui.lua
@@ -2,11 +2,13 @@ local UI = {}
 
 function UI.Init(Aimlock)
 
+--INTERFACE GERAL_____________________________________________________
     local player = game.Players.LocalPlayer
     local playerGui = player:WaitForChild("PlayerGui")
 
     local screenGui = Instance.new("ScreenGui")
     screenGui.Name = "MinhaInterfaceExecutor"
+    screenGui.ResetOnSpawn = false
     screenGui.Parent = playerGui
 
     local quadroAzul = Instance.new("Frame")
@@ -27,12 +29,28 @@ function UI.Init(Aimlock)
     }
     gradient.Rotation = 45
     gradient.Parent = quadroAzul
+--____________________________________________________________________
 
+
+--Minimizar Menu Do Script____________________________________________
+
+    local MinimizarMenu = Instance.new("TextButton")
+    MinimizarMenu.Size = UDim2.new(0,70,0,25)
+    MinimizarMenu.Position = UDim2.new(1,0,1,0)
+    MinimizarMenu.BackgroundColor3 = Color3.fromRGB(10,40,70)
+    MinimizarMenu.Text = "MINIMIZE"
+    MinimizarMenu.TextColor3 = Color3.fromRGB(0,255,255)
+    MinimizarMenu.Parent = quadroAzul
+    AimLockButton.BackgroundTransparency = 0
+
+--____________________________________________________________________
+
+--Botão de desativar/ativar AimBot____________________________________
     local AimLockButton = Instance.new("TextButton")
     AimLockButton.Size = UDim2.new(0, 100, 0, 40)
     AimLockButton.Position = UDim2.new(0.5, -50, 0.5, -25)
     AimLockButton.BackgroundColor3 = Color3.fromRGB(70, 0, 0)
-    AimLockButton.Text = "AIM LOCK"
+    AimLockButton.Text = "AIM BOT"
     AimLockButton.TextColor3 = Color3.fromRGB(0, 255, 255)
     AimLockButton.Parent = quadroAzul
     AimLockButton.BackgroundTransparency = 0
@@ -43,13 +61,15 @@ function UI.Init(Aimlock)
 
     AimLockButton.MouseButton1Click:Connect(function()
         Aimlock.TurnOnOrOff(not Aimlock.IsActive())
-        print("Estado agora:", Aimlock.IsActive())
         if Aimlock.IsActive() then 
             AimLockButton.BackgroundColor3 = Color3.fromRGB(0, 70, 0)
         else
             AimLockButton.BackgroundColor3 = Color3.fromRGB(70, 0, 0)
         end
     end)
+--__________________________________________________________________
+
+
 
 end
 
